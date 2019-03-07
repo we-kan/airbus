cc4cf0cfdcaf0fcbf043110da69fa81d0fb5b5e2
kaushik kumar <kaushik.kumar@adcuratio.com>
2019-03-07 10:51:25 +0530

    add apps in settings

--- airbus/settings.py
+++ airbus/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'aircraft',
+    'airport',
+    'flight',
+    'flight_route'
 ]
 
 MIDDLEWARE = [
